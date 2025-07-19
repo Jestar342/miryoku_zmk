b23e31631a86953cfeb4aeb7a41fe2de817c7feb
Jestar342 <457158+Jestar342@users.noreply.github.com>
2025-07-19 20:26:47 +0100

    fix parenthesis mappings

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -5,7 +5,7 @@
 &kp LBRC,          &kp AMPS,          &kp ASTRK,         &kp LS(N3),        &kp RBRC,          U_NA,              &u_to_U_BASE,      &u_to_U_EXTRA,     &u_to_U_TAP,       U_BOOT,            \
 &kp COLON,         &kp DLLR,          &kp PRCNT,         &kp CARET,         &kp PLUS,          U_NA,              &kp LSHFT,         &kp LCTRL,         &kp LALT,          &kp LGUI,          \
 &kp LS(NUHS),      &kp EXCL,          &kp LS(SQT),       &kp NUHS,          &kp LS(NUBS),      U_NA,              &u_to_U_SYM,       &u_to_U_MOUSE,     &kp RALT,          U_NA,              \
-U_NP,              U_NP,              &kp LS(LBKT),      &kp LS(RBKT),      &kp LS(MINUS),     U_NA,              U_NA,              U_NA,              U_NP,              U_NP
+U_NP,              U_NP,              &kp LS(N9),        &kp LS(N0),        &kp LS(MINUS),     U_NA,              U_NA,              U_NA,              U_NP,              U_NP
 
 #define MIRYOKU_LAYER_NUM \
 &kp LBKT,          &kp N7,            &kp N8,            &kp N9,            &kp RBKT,          U_NA,              &u_to_U_BASE,      &u_to_U_EXTRA,     &u_to_U_TAP,       U_BOOT,            \
